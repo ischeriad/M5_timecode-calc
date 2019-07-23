e81f7a0a1514e983cc34d34913570bbdb6739dc0
ischeriad <1580928+ischeriad@users.noreply.github.com>
2019-07-23 19:13:28 +0200

    clean up

--- timecode-calc.py
+++ timecode-calc.py
@@ -1,22 +1,29 @@
-    # TODO:
+# TODO: parse different input
 
 # 11:23:43:24
 # 1:::12 -> 01:00:00:12
 # 10:5:10: -> 10:05:10:00
 
+# TODO: handle more frame rates
+
 # 25, 50
 # 30, 60, 29.97 (30/1001), 59.95 (60/1001)
 # 24, 23.98 (24/1001)
 
-#
-# TODO: jkhkjh
 
 ###############
 #### SETUP ####
 ###############
 
 fps = 25
-myTC = dict()
+inputTC = {
+    'hours': '00',
+    'minutes': '00',
+    'seconds': '00',
+    'frames': '00'
+}
+
+# TODO: learn how to use unit tests
 
 ## Testcases
 
@@ -30,8 +37,8 @@ test_tc = "11234324"
 #### FUNCTIONS ####
 ###################
 
-## Timecode to Frames
-## int tc2frames(dict tc, int fps)
+# Timecode to Frames
+# int tc2frames(dict tc, int fps)
 def tc2frames(tc, fps):
     frames = tc['frames']
     frames += tc['seconds'] * fps
@@ -40,8 +47,9 @@ def tc2frames(tc, fps):
 
     return frames
 
-## Frames to Timecode
-## str frames2tc(int frames, int fps)
+
+# Frames to Timecode
+# str frames2tc(int frames, int fps)
 def frames2tc(frames, fps):
     framesTC    = frames % fps
     secondsTC   = frames // fps % 60
@@ -53,22 +61,22 @@ def frames2tc(frames, fps):
 
 # Timecode string to dict, slice 2 from the end
 
-myTC['frames'] = int(test_tc[-2:])
+inputTC['frames'] = int(test_tc[-2:])
 test_tc = test_tc[:-2]
-myTC['seconds'] = int(test_tc[-2:])
+inputTC['seconds'] = int(test_tc[-2:])
 test_tc = test_tc[:-2]
-myTC['minutes'] = int(test_tc[-2:])
+inputTC['minutes'] = int(test_tc[-2:])
 test_tc = test_tc[:-2]
-myTC['hours'] = int(test_tc[-2:])
+inputTC['hours'] = int(test_tc[-2:])
 test_tc = test_tc[:-2]
 
 
 #### TESTS
 
 # TC to Frames
-myTC2frames = tc2frames(myTC, fps)
-print("myTC 2 Frames: " + str(myTC2frames))
+myTC2frames = tc2frames(inputTC, fps)
+print("myTC to Frames: " + str(myTC2frames))
 
 # Frames to TC
-frames2tc(myTC2frames, 25)
-print("Back to TC: " + str(frames2tc(myTC2frames, 25)))
+frames2tc(myTC2frames, fps)
+print("    Back to TC: " + str(frames2tc(myTC2frames, fps)))
