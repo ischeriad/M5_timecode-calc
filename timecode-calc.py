07fe3200a0f8ec2b3166bfcb081d8ad3dcec94b2
ischeriad <1580928+ischeriad@users.noreply.github.com>
2019-07-26 21:55:56 +0200

    trying new things. class TimeCode added

--- timecode-calc.py
+++ timecode-calc.py
@@ -30,8 +30,10 @@ inputTC = {
 test_tc = "09592924"
 # test_tc = "10000000"
 test_tc = "01010101"
-test_tc = "11234324"
+# test_tc = "11234324"
 
+
+input_str = test_tc
 
 ###################
 #### FUNCTIONS ####
@@ -59,16 +61,42 @@ def frames2tc(frames, fps):
     return str(hoursTC).zfill(2) + ":" + str(minutesTC).zfill(2) + ":" + str(secondsTC).zfill(2) + ":" +str(framesTC).zfill(2)
 
 
+# Cleanup timecode:
+# remove colons, pad with zeros
+# str clean_tc(str input_str)
+def clean_tc(input_str):
+    # timecode string as list
+    input_list = list(input_str)
+    clean_tc = ""
+
+    while bool(input_list):
+        last_item = input_list.pop()
+        print("Last item: " + last_item)
+
+        for item in ("frames", "seconds", "minutes", "hours"):
+            print("The item: " + item)
+            item = list("00")
+            for digit in range (-1, -2):
+                print("The digit: " + digit)
+                if last_item == ":":
+                    break
+                else:
+                    clean_tc = last_item + clean_tc
+            # print("building clean tc: " + clean_tc)
+
+clean_tc(input_str)
+
 # Timecode string to dict, slice 2 from the end
 
-inputTC['frames'] = int(test_tc[-2:])
-test_tc = test_tc[:-2]
-inputTC['seconds'] = int(test_tc[-2:])
-test_tc = test_tc[:-2]
-inputTC['minutes'] = int(test_tc[-2:])
-test_tc = test_tc[:-2]
-inputTC['hours'] = int(test_tc[-2:])
-test_tc = test_tc[:-2]
+inputTC['frames'] = int(input_str[-2:])
+input_str = input_str[:-2]
+inputTC['seconds'] = int(input_str[-2:])
+input_str = input_str[:-2]
+inputTC['minutes'] = int(input_str[-2:])
+input_str = input_str[:-2]
+inputTC['hours'] = int(input_str[-2:])
+input_str = input_str[:-2]
+
 
 
 #### TESTS
@@ -80,3 +108,56 @@ print("myTC to Frames: " + str(myTC2frames))
 # Frames to TC
 frames2tc(myTC2frames, fps)
 print("    Back to TC: " + str(frames2tc(myTC2frames, fps)))
+
+
+#### trying OOP
+
+class TimeCode():
+    def __init__(self, fps, hours, minutes, seconds, frames):
+        """Initialize TimeCode"""
+        self.fps = fps
+        self.hours = list(str(hours))
+        self.minutes = list(str(minutes))
+        self.seconds = list(str(seconds))
+        self.frames = list(str(frames))
+
+    def return_absolute_frames(self):
+
+        fra = int(''.join(str(each) for each in self.frames))
+        sec = int(''.join(str(each) for each in self.seconds))
+        min = int(''.join(str(each) for each in self.minutes))
+        hrs = int(''.join(str(each) for each in self.hours))
+
+        frames = fra
+        frames += sec * self.fps
+        frames += min * self.fps * 60
+        frames += hrs * self.fps * 60 * 60
+
+        return frames
+
+    def return_tc_string(self):
+
+        return ':'.join([self.hours, self.minutes, self.seconds, self.frames])
+
+    def set_absolute_frames(self, absolute_frames):
+        # framesTC = frames % fps
+        # secondsTC = frames // fps % 60
+        # minutesTC = frames // fps // 60 % 60
+        # hoursTC = frames // fps // 60 // 60
+
+        self.frames = str(absolute_frames % self.fps).zfill(2)
+        self.seconds = str(absolute_frames // self.fps % 60).zfill(2)
+        self.minutes = str(absolute_frames // self.fps // 60 % 60).zfill(2)
+        self.hours = str(absolute_frames // self.fps // 60 // 60).zfill(2)
+
+my_tc_25 = TimeCode(25, "01", 00, 00, 10)
+my_tc_24 = TimeCode(24, 0, 0, 0, 0)
+
+my_tc_25.set_absolute_frames(90000)
+
+my_tc_24.set_absolute_frames(my_tc_25.return_absolute_frames())
+
+print(my_tc_25.return_absolute_frames())
+
+print(my_tc_25.return_tc_string() + " @ " + str(my_tc_25.fps) + "fps")
+print(my_tc_24.return_tc_string() + " @ " + str(my_tc_24.fps) + "fps")
